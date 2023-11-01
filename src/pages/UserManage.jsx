8ab32c47a5fe89e47facd84ab48dd0aee17da4f7
agunggst <rqz.agastya@gmail.com>
2023-11-02 03:09:33 +0700

    checkpoint

--- src/pages/UserManage.jsx
+++ src/pages/UserManage.jsx
@@ -1,7 +1,59 @@
+import CustomTable from "../components/CustomTable"
+import "./style/UserManage.css"
+import { Avatar, TextField } from "@mui/material"
+
 const UserManage = () => {
+  const tableHeads = ['User Id', 'Username', 'Full Name', 'Avatar', 'Email', 'Address']
+  const users = [
+    {
+      id: 1,
+      username: 'admin1',
+      fullname: 'admin 123 123',
+      avatar: <Avatar src="https://pbs.twimg.com/media/FjU2lkcWYAgNG6d.jpg"/>,
+      email: 'asd@asd.asd',
+      address: 'Lorem ipum dolor sit amet, consectetur adipiscing el'
+    },
+    {
+      id: 1,
+      username: 'admin1',
+      fullname: 'admin 123 123',
+      avatar: <Avatar src="https://pbs.twimg.com/media/FjU2lkcWYAgNG6d.jpg"/>,
+      email: 'asd@asd.asd',
+      address: 'Lorem ipum dolor sit amet, consectetur adipiscing el'
+    },
+    {
+      id: 1,
+      username: 'admin1',
+      fullname: 'admin 123 123',
+      avatar: <Avatar src="https://pbs.twimg.com/media/FjU2lkcWYAgNG6d.jpg"/>,
+      email: 'asd@asd.asd',
+      address: 'Lorem ipum dolor sit amet, consectetur adipiscing el'
+    },
+    {
+      id: 1,
+      username: 'admin1',
+      fullname: 'admin 123 123',
+      avatar: <Avatar src="https://pbs.twimg.com/media/FjU2lkcWYAgNG6d.jpg"/>,
+      email: 'asd@asd.asd',
+      address: 'Lorem ipum dolor sit amet, consectetur adipiscing el'
+    },
+    {
+      id: 1,
+      username: 'admin1',
+      fullname: 'admin 123 123',
+      avatar: <Avatar src="https://pbs.twimg.com/media/FjU2lkcWYAgNG6d.jpg"/>,
+      email: 'asd@asd.asd',
+      address: 'Lorem ipum dolor sit amet, consectetur adipiscing el'
+    },
+  ]
   return (
     <div className="user-manage">
-      user manage
+      <div className="utilities">
+        <TextField id="standard-basic" label="Search By Id" variant="standard" sx={{ width: '300px' }} />
+      </div>
+      <div className="table">
+        <CustomTable data={users} heads={tableHeads}/>
+      </div>
     </div>
   )
 }
